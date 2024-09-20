86c92612afb99c8193ab0fbfa044c91f57bb1128
Roman Gilg <romangg@manjaro.org>
2024-09-20 23:44:13 +0200

    add tailwind

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,16 @@
 <template>
-  <h1>Control Panel</h1>
-  <p>
-    <strong>Current route path:</strong> {{ $route.fullPath }}
-  </p>
-  <nav>
-    <RouterLink to="/">Home </RouterLink>
-  </nav>
-  <main>
-    <RouterView />
-  </main>
+  <div>
+    <h1 class="text-3xl">Control Panel</h1>
+    <p>
+      <strong>Current route path:</strong> {{ $route.fullPath }}
+    </p>
+    <nav>
+      <RouterLink to="/">Home </RouterLink>
+    </nav>
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
 
 <style scoped>
@@ -17,9 +19,11 @@
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #e80000aa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
